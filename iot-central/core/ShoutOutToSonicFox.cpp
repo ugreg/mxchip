f09a0d7273280463fd0a3d87854db03a3ded0ca9
Greg Degruy <gregory.degruy@gmail.com>
2018-12-07 20:02:16 -0800

    + some iot central logic, need to port over the rest

---
+++ iot-central/core/ShoutOutToSonicFox.cpp
@@ -0,0 +1,4 @@
+ShoutOutToSonicFox::Initialize()
+{
+    Serial.begin(9600);
+}
---
+++ iot-central/core/ShoutOutToSonicFox.h.h
@@ -0,0 +1,12 @@
+#ifndef SHOUTOUTTOSONICFOX_H.H
+#define SHOUTOUTTOSONICFOX_H.H
+
+class ShoutOutToSonicFox
+{
+    ShoutOutToSonicFox() { }
+    public:
+        static bool Initialize();
+        static bool loop();
+};
+
+#endif /* SHOUTOUTTOSONICFOX_H.H */
