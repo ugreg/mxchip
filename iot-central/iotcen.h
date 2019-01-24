7811b8e99a4fc082a0981b9f1288ccb41445fbae
gregdegruy <gregory.degruy@gmail.com>
2019-01-24 09:45:26 -0800

    + c struct properties

--- iot-central/iotcen.h
+++ iot-central/iotcen.h
@@ -10,12 +10,33 @@
 #endif
 
 #ifndef _cpluscplus
+// TODO: consider replacing extern c with cpp, what are the implications
+// https://stackoverflow.com/questions/31903594/typedef-struct-in-c-vs-c
 extern "C"
 {
+    #endif
     typedef struct IOT_C_HTTP_PROXY_OPTIONS_TAG
     {
-
-    }
+        const char *host_address;
+        int port;
+        const char *username;
+        const char *password;
+    } IOT_HTTP_PROXY_OPTIONS;
 
     typedef struct IOTCallbackInfo_TAG
+    {
+        const char *event_name;
+        const char *tag;
+        const char *payload;
+        unsigned payload_length;
+
+        void *app_context;
+
+        int status_code;
+        void *callback_response;
+    } IOTCallbackInfo;
+
+    typedef void* IOTContext;
+
+    // ᕕ( ᐛ )ᕗ memory addresses!!!
 }
