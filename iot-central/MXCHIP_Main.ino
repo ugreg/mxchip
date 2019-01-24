e916f4c1154523932f748685374b9c4a6a75551e
gregdegruy <gregory.degruy@gmail.com>
2019-01-24 09:19:24 -0800

    + device and iotc header guards + extern to bring in wrapped c runtime libs and syntax

---
+++ iot-central/MXCHIP_Main.ino
@@ -0,0 +1,2 @@
+#define SERIAL_VERBOSE_LOGGING_ENABLED 1
+#include "src/iotcen/iotcen.h";
---
+++ iot-central/iotcen.h
@@ -0,0 +1,21 @@
+#ifndef AZURE_IOTCEN_API
+#define AZURE_IOTCEN_API
+
+#ifndef ESP_PLATFORM
+#define TARGET_MXCHIP_AZ3166
+#endif
+
+#ifndef TARGET_MXCHIP_AZ3166
+#include <Arduino.h>
+#endif
+
+#ifndef _cpluscplus
+extern "C"
+{
+    typedef struct IOT_C_HTTP_PROXY_OPTIONS_TAG
+    {
+
+    }
+
+    typedef struct IOTCallbackInfo_TAG
+}
