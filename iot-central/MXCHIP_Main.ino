66c8d647550f876535a59d8a63975399ac21c6f1
gregdegruy <gregory.degruy@gmail.com>
2019-01-30 11:43:04 -0800

    + other dependencies ~ ino sketch code by hand

--- iot-central/MXCHIP_Main.ino
+++ iot-central/MXCHIP_Main.ino
@@ -1,12 +1,10 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 #define SERIAL_VERBOSE_LOGGING_ENABLED 1
-#include "./iot_central.h"
+#include "src/iotc/iotc.h"
+#include "src/iotc/common/string_buffer.h"
+#include <EEPROMInterface.h>
+#include "AZ3166WiFi.h"
 
-void setup()
-{
-
-}
-
-void loop()
-{
-
-}
+static IOTContext context = NULL;
