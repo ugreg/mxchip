50e122b26385ab7feae64d82f6de074ab8c785f5
gregdegruy <gregory.degruy@gmail.com>
2019-01-30 16:38:12 -0800

    + even listener for command and wifi connection callbacks

--- iot-central/MXCHIP_Main.ino
+++ iot-central/MXCHIP_Main.ino
@@ -13,3 +13,35 @@ static IOTContext context = NULL;
 #define WIFI_PASSWORD "pwnnubz";
 
 IOTConnectType connectType = IOTC_CONNECT_CONNECTION_STRING;
+const char *scopeId = "";
+const char *deviceId = "";
+const char *deviceKey = "";
+
+// PRIMARY/SECONDARY KEY ?? (DPS)
+// Uncomment below to Use DPS Symm Key (primary/secondary key..)
+// IOTConnectType connectType = IOTC_CONNECT_SYMM_KEY;
+// const char* scopeId = "<Enter ScopeID>";
+// const char* deviceId = "<Enter DeviceId>";
+// const char* deviceKey = "<Enter Primary or Secondary Device Key here>";
+
+static bool isConnected = false;
+
+void onEvent(IOTContext iotContext, IOTCallbackInfo *callbackInfo)
+{
+    if (strcmp(callbackInfo->eventName, "ConnectionStatus") == 0) {
+        LOG_VERBOSE("Is connected ? %s (%d)", callbackInfo->statusCode == IOTC_CONNECTION_OK ? "Yes" : "No",
+        callbackInfo->statusCode);
+        isConnected = callbackInfo->statusCode == IOTC_CONNECTION_OK;
+    }
+
+    AzureIOT::StringBuffer stringBuffer;
+    if (callbackInfo->payloadLength > 0) {
+        stringBuffer.initialize(callbackInfo->payload, callbackInfo->payloadLength);
+    }
+
+    LOG_VERBOSE("- [%s] event was received. Payload => %s", callbackInfo->eventName, buffer.getLength() ? *buffer : "EMPTY");
+
+    if (strcmp(callbackInfo->eventName, "Command") == 0) {
+        LOG_VERBOSE("Command name was => %s\r\n", callbackInfo->tag);
+    }
+}
